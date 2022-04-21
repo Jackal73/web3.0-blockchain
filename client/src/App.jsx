94b0dd3ee0319b99326cf628795c2e0c77ef0fa7
Shawn Kebel <fxrender@outlook.com>
2022-04-20 19:03:41 -0500

    :sparkles: Add `Navigation Bar` w/ mobile menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
+import { Navbar, Footer, Welcome, Services, Transactions } from './components';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
